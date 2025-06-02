628be476cdb6439b70a3e5d18d55c7ed7a87c407
MIrgval <gmitusovserega@gmail.com>
2025-06-03 02:13:51 +0700

    b

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: {
-    container: { center: true, screens: { '2xl': 'auto' } },
-    extend: { colors: { dark: '#0d0d0d' } }
-  },
-  plugins: []
-};
